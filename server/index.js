97e0825b7ddd98b93543fe761f94bf2712ea7264
kfulkerson-pcg <kfulkerson@phxcapitalgroup.com>
2023-01-20 10:07:05 -0800

    project init for server + client :alien:

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+
+import connectDB from "./mongodb/connect.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+
+app.get("/", async (req, res) => {
+  res.status(200).json({
+    message: "Hello from DALL.E!",
+  });
+});
+
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(9000, () => console.log("Server started on port 9000"));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
