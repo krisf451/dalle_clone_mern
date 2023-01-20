cb0d0036c4a7c69deacead2d52be06ccf4e90c19
kfulkerson-pcg <kfulkerson@phxcapitalgroup.com>
2023-01-20 15:23:33 -0800

    trying to get backend deployed on render

--- server/mongodb/connect.js
+++ server/mongodb/connect.js
@@ -1,12 +1,12 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const connectDB = (url) => {
-  mongoose.set("strictQuery", true);
+  mongoose.set('strictQuery', true);
   mongoose
     .connect(url)
-    .then(() => console.log("Connected to MongoDB"))
+    .then(() => console.log('Connected to MongoDB'))
     .catch((err) => {
-      console.error("failed to connect with mongo");
+      console.error('failed to connect with mongo');
       console.error(err);
     });
 };
