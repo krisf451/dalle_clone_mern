5a6daecbcb842090725d6321b234ea46dc6711e4
kfulkerson-pcg <kfulkerson@phxcapitalgroup.com>
2023-01-20 13:18:51 -0800

    feat: add Post model and postRoutes, add dalleRoutes for AI image generation, add eslint config to server environment

--- server/index.js
+++ server/index.js
@@ -1,25 +1,30 @@
-import express from "express";
-import * as dotenv from "dotenv";
-import cors from "cors";
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
 
-import connectDB from "./mongodb/connect.js";
+import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: '50mb' }));
 
-app.get("/", async (req, res) => {
+app.use('/api/v1/posts', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
+
+app.get('/', async (req, res) => {
   res.status(200).json({
-    message: "Hello from DALL.E!",
+    message: 'Hello from DALL.E!',
   });
 });
 
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(9000, () => console.log("Server started on port 9000"));
+    app.listen(9000, () => console.log('Server started on port 9000'));
   } catch (error) {
     console.log(error);
   }
